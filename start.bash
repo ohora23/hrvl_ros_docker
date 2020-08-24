56d4eaac3acc4d50f4d1507a4fe44550abafa7d3
Prof.Jeong-Ki Yoo <jkyoo@dju.ac.kr>
2020-08-25 00:44:00 +0900

    changed start.bash - dockerhub name changed

--- start.bash
+++ start.bash
@@ -4,11 +4,11 @@ XAUTH=/tmp/.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-nvidia-docker run --priviledged -it \
+nvidia-docker run -it \
         --volume=$XSOCK:$XSOCK:rw \
         --volume=$XAUTH:$XAUTH:rw \
         --env="XAUTHORITY=${XAUTH}" \
         --env="DISPLAY" \
         --env="UID=`id -u $who`" \
         --env="UID=`id -g $who`" \
-    hrvl/ros-melodic:cuda10.2-cudnn7-vscode-pytorch 
+     ohora23/hrvl_devel_env:ros-melodic-cuda10.1-cudnn7-all-ubuntu18.04
