2e98b1f3f5f354ad551b04ebe430d8ba2ededa57
Jeong-Ki Yoo <ohora23@gmail.com>
2021-11-04 10:01:34 +0900

    Add files via upload

--- start_with.bash
+++ start_with.bash
@@ -1,25 +1,20 @@
 #!/bin/bash
 XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-touch $XAUTH
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-nvidia-docker run -it \
-        --net host \
-        --hostname=hrvl-AERO\
-        --privileged=true \
-        --volume=$XSOCK:$XSOCK:rw \
-        --volume=$XAUTH:$XAUTH:rw \
-        --volume=/home/hrvl:/home/ros/data:rw \
-        --volume=${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
-        --group-add $(getent group audio | cut -d: -f3) \
-        --device /dev/snd \
-        --device /dev/video0 \
-        --env="PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native" \
-        --env="XAUTHORITY=${XAUTH}" \
-        --env="DISPLAY" \
-        --env="UID=`id -u $who`" \
-        --env="UID=`id -g $who`" \
-	@1
-
-
+docker run -it \
+	--gpus all\
+	--network=host\
+	--privileged \
+	--volume=$XSOCK:$XSOCK:rw \
+	--volume=${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+	--volume="/home/$USER:/home/ros/$USER"\
+	--volume="/media:/media:rw" \
+	--volume="/dev:/dev:rw" \
+	--volume="/sys:/sys:rw" \
+	--group-add $(getent group audio | cut -d: -f3) \
+	--device /dev/snd \
+	--device /dev/dri \
+	--env="PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native" \
+	--env="DISPLAY=${DISPLAY}" \
+	--volume="$HOME/.Xauthority:/home/ros/.Xauthority:rw"\
+	$1
